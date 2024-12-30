ac90b922d34f7790541e50ca31ee8ca4b95e8f23
zzyeah <541767316@qq.com>
2024-12-30 18:04:35 +0800

    feat(Login): :sparkles: 构建登陆面板的结构

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,5 +1,16 @@
-import { Modal, Radio, RadioChangeEvent } from "antd";
-import React, { useState } from "react";
+import {
+  Button,
+  Checkbox,
+  Col,
+  Form,
+  FormInstance,
+  Input,
+  Modal,
+  Radio,
+  RadioChangeEvent,
+  Row,
+} from "antd";
+import React, { Dispatch, SetStateAction, useRef, useState } from "react";
 import styles from "../styles/LoginForm.module.css";
 
 export interface LoginFormProps {
@@ -12,10 +23,45 @@ export enum LoginFormRadioButtonTypes {
   REGISTER,
 }
 
+export interface IBaseInfo {
+  loginId: string;
+  captcha: string;
+}
+
+export interface LoginInfo extends IBaseInfo {
+  loginPwd: string;
+  remember: boolean;
+}
+
+export interface RegisterInfo extends IBaseInfo {
+  nickname: string;
+}
+
 function LoginForm(props: LoginFormProps) {
   const { isShow, closeModal } = props;
-  const { Group, Button } = Radio;
+  const { Group: RadioGroup, Button: RadioButton } = Radio;
+  const { Item: FormItem } = Form;
+  const { Password } = Input;
   const [value, setValue] = useState(LoginFormRadioButtonTypes.LOGIN);
+  const loginFormRef = useRef<FormInstance>();
+  const registerFormRef = useRef<FormInstance>();
+
+  // 登录表单的状态数据
+  const [loginInfo, setLoginInfo] = useState<LoginInfo>({
+    loginId: "",
+    loginPwd: "",
+    captcha: "",
+    remember: false,
+  });
+
+  // 注册表单的状态数据
+  const [registerInfo, setRegisterInfo] = useState<RegisterInfo>({
+    loginId: "",
+    nickname: "",
+    captcha: "",
+  });
+
+  const [captcha, setCaptcha] = useState<string>(null);
 
   function handleOk(): void {
     throw new Error("Function not implemented.");
@@ -25,13 +71,266 @@ function LoginForm(props: LoginFormProps) {
     const { value } = e.target;
     setValue(value);
   }
+  function loginHandle(values: any): void {
+    throw new Error("Function not implemented.");
+  }
+
+  function updateInfo<T extends IBaseInfo, K extends keyof T>(
+    loginInfo: T,
+    value: T[K],
+    key: K,
+    setLoginInfo: Dispatch<SetStateAction<T>>
+  ): void {
+    const obj = { ...loginInfo };
+    obj[key] = value;
+    setLoginInfo(obj);
+  }
+
+  function captchaClickHandle(): void {
+    throw new Error("Function not implemented.");
+  }
 
   function LoginContainer() {
-    return <>登陆面板</>;
+    return (
+      <div className={styles.container}>
+        <Form
+          name="basic1"
+          autoComplete="off"
+          onFinish={loginHandle}
+          ref={loginFormRef}
+        >
+          <FormItem
+            label="登录账号"
+            name="loginId"
+            rules={[
+              {
+                required: true,
+                message: "请输入账号",
+              },
+            ]}
+          >
+            <Input
+              placeholder="请输入你的登录账号"
+              value={loginInfo.loginId}
+              onChange={(e) =>
+                updateInfo(loginInfo, e.target.value, "loginId", setLoginInfo)
+              }
+            />
+          </FormItem>
+
+          <FormItem
+            label="登录密码"
+            name="loginPwd"
+            rules={[
+              {
+                required: true,
+                message: "请输入密码",
+              },
+            ]}
+          >
+            <Password
+              placeholder="请输入你的登录密码，新用户默认为123456"
+              value={loginInfo.loginPwd}
+              onChange={(e) =>
+                updateInfo(loginInfo, e.target.value, "loginPwd", setLoginInfo)
+              }
+            />
+          </FormItem>
+
+          {/* 验证码 */}
+          <FormItem
+            name="logincaptcha"
+            label="验证码"
+            rules={[
+              {
+                required: true,
+                message: "请输入验证码",
+              },
+            ]}
+          >
+            <Row align="middle">
+              <Col span={16}>
+                <Input
+                  placeholder="请输入验证码"
+                  value={loginInfo.captcha}
+                  onChange={(e) =>
+                    updateInfo(
+                      loginInfo,
+                      e.target.value,
+                      "captcha",
+                      setLoginInfo
+                    )
+                  }
+                />
+              </Col>
+              <Col span={6}>
+                <div
+                  className={styles.captchaImg}
+                  onClick={captchaClickHandle}
+                  dangerouslySetInnerHTML={{ __html: captcha }}
+                ></div>
+              </Col>
+            </Row>
+          </FormItem>
+
+          <FormItem
+            name="remember"
+            wrapperCol={{
+              offset: 5,
+              span: 16,
+            }}
+          >
+            <Checkbox
+              onChange={(e) =>
+                updateInfo(
+                  loginInfo,
+                  e.target.checked,
+                  "remember",
+                  setLoginInfo
+                )
+              }
+              checked={loginInfo.remember}
+            >
+              记住我
+            </Checkbox>
+          </FormItem>
+
+          <FormItem
+            wrapperCol={{
+              offset: 5,
+              span: 16,
+            }}
+          >
+            <Button
+              type="primary"
+              htmlType="submit"
+              style={{ marginRight: 20 }}
+            >
+              登录
+            </Button>
+            <Button type="primary" htmlType="submit">
+              重置
+            </Button>
+          </FormItem>
+        </Form>
+      </div>
+    );
   }
 
   function RegisterContainer() {
-    return <>注册面板</>;
+    function registerHandle(values: any): void {
+      throw new Error("Function not implemented.");
+    }
+
+    function captchaClickHandle(): void {
+      throw new Error("Function not implemented.");
+    }
+
+    return (
+      <div className={styles.container}>
+        <Form
+          name="basic2"
+          autoComplete="off"
+          ref={registerFormRef}
+          onFinish={registerHandle}
+        >
+          <FormItem
+            label="登录账号"
+            name="loginId"
+            rules={[
+              {
+                required: true,
+                message: "请输入账号，仅此项为必填项",
+              },
+              // 验证用户是否已经存在
+              // { validator: checkLoginIdIsExist },
+            ]}
+            validateTrigger="onBlur"
+          >
+            <Input
+              placeholder="请输入账号"
+              value={registerInfo.loginId}
+              onChange={(e) =>
+                updateInfo(
+                  registerInfo,
+                  e.target.value,
+                  "loginId",
+                  setRegisterInfo
+                )
+              }
+            />
+          </FormItem>
+
+          <FormItem label="用户昵称" name="nickname">
+            <Input
+              placeholder="请输入昵称，不填写默认为新用户xxx"
+              value={registerInfo.nickname}
+              onChange={(e) =>
+                updateInfo(
+                  registerInfo,
+                  e.target.value,
+                  "nickname",
+                  setRegisterInfo
+                )
+              }
+            />
+          </FormItem>
+
+          <FormItem
+            name="registercaptcha"
+            label="验证码"
+            rules={[
+              {
+                required: true,
+                message: "请输入验证码",
+              },
+            ]}
+          >
+            <Row align="middle">
+              <Col span={16}>
+                <Input
+                  placeholder="请输入验证码"
+                  value={registerInfo.captcha}
+                  onChange={(e) =>
+                    updateInfo(
+                      registerInfo,
+                      e.target.value,
+                      "captcha",
+                      setRegisterInfo
+                    )
+                  }
+                />
+              </Col>
+              <Col span={6}>
+                <div
+                  className={styles.captchaImg}
+                  onClick={captchaClickHandle}
+                  dangerouslySetInnerHTML={{ __html: captcha }}
+                ></div>
+              </Col>
+            </Row>
+          </FormItem>
+
+          <FormItem
+            wrapperCol={{
+              offset: 5,
+              span: 16,
+            }}
+          >
+            <Button
+              type="primary"
+              htmlType="submit"
+              style={{ marginRight: 20 }}
+            >
+              注册
+            </Button>
+            <Button type="primary" htmlType="submit">
+              重置
+            </Button>
+          </FormItem>
+        </Form>
+      </div>
+    );
   }
 
   let container = null;
@@ -49,25 +348,25 @@ function LoginForm(props: LoginFormProps) {
         onOk={handleOk}
         onCancel={closeModal}
       >
-        <Group
+        <RadioGroup
           value={value}
           onChange={onChange}
           className={styles.radioGroup}
           buttonStyle="solid"
         >
-          <Button
+          <RadioButton
             value={LoginFormRadioButtonTypes.LOGIN}
             className={styles.radioButton}
           >
             登陆
-          </Button>
-          <Button
+          </RadioButton>
+          <RadioButton
             value={LoginFormRadioButtonTypes.REGISTER}
             className={styles.radioButton}
           >
             注册
-          </Button>
-        </Group>
+          </RadioButton>
+        </RadioGroup>
         {/* 下面需要显示对应功能的表单 */}
         {container}
       </Modal>
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './index.css'
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
@@ -6,9 +7,8 @@ import zhCN from "antd/es/locale/zh_CN";
 import { ConfigProvider } from "antd";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import '@ant-design/v5-patch-for-react-19';
-const root = createRoot(document.getElementById("root") as HTMLElement);
 
+const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <Provider store={store}>
     <BrowserRouter>
