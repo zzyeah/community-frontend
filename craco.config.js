4fb4ddab5c9fac28bdf806c195247f8cd7595cf3
zzyeah <541767316@qq.com>
2025-01-02 15:07:01 +0800

    feat(Config): :bug: 修改 craco config
    修改引入路径

--- craco.config.js
+++ craco.config.js
@@ -4,12 +4,8 @@ module.exports = {
   webpack: {
     alias: {
       "@": path.resolve(__dirname, "src"),
-      "@styles": path.resolve(__dirname, "src/styles"),
-      "@types": path.resolve(__dirname, "src/types"),
-      "@redux": path.resolve(__dirname, "src/redux"),
     },
     configure: (webpackConfig, { env, paths }) => {
-      // webpackConfig.entry = './path/to/my/entry/file.js';
       return webpackConfig;
     },
   },
